c338df2231c0ac36f8d2c8f97369123cbb90d755
Khaled Mohammed <khaled.mak94@gmail.com>
2016-10-19 16:55:31 +0530

    form class created

---
+++ formClass.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user-1
+ * Date: 10/19/2016
+ * Time: 4:15 PM
+ */
+
+class MyForm {
+	const NAME = 'stdForm';
+	protected $elements = [];
+	protected $name ;
+	public $valid = false;
+
+	public function setName($name = null){
+		if($name){
+			$this->name=$name;
+		}else{
+			$this->name = self::NAME;
+		}
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+}
